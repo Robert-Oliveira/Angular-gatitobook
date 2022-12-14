f1b7cc4dd16f824f8dba5fc78ac7f0d81247c48a
Robert Oliveira <86692878+Robert-Oliveira@users.noreply.github.com>
2022-12-14 20:26:01 -0300

    adicionado uma validação para checar se o nome do usuario já existe no banco

--- gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,6 +1,7 @@
 import { NovoUsuario } from './novo-usuario';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,12 @@ export class NovoUsuarioService {
     return this.httpClient.post(
       'http://localhost:3000/user/signup',
       novoUsuario
+    );
+  }
+
+  verificaUsuarioExistente(nomeUsuario: String): Observable<any> {
+    return this.httpClient.get(
+      `http://localhost:3000/user/exists/${nomeUsuario}`
     );
   }
 }
